6d454722f247551c3b4711d30830d8c1645886ad
Charlotte Lui <charlotteytlui@gmail.com>
2020-04-01 13:53:44 +0300

    task validation

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,12 @@
 class Task < ApplicationRecord
+  belongs_to :bucket
+
+  validates :title, presence: true
+  validate :due_date_cannot_be_in_the_past
+
+  def due_date_cannot_be_in_the_past
+    if due_date < Date.today
+      errors.add(:due_date, "cannot be in the past")
+    end
+  end
 end
